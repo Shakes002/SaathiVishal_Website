4f78c265f46ba2d6578d687ae6175dc507c764b1
Abhishek Yadav <shakesheartcore@gmail.com>
2022-01-06 12:55:21 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,43 @@
+/*---Toggle Sidemenu---*/
+let navbar = document.querySelector("nav");
+let myNav = document.getElementById("nav-bar");
+let navLinks = myNav.querySelectorAll("li");
+let hamMenu = document.getElementById("ham-menu");
+hamMenu.addEventListener("click", () => {
+    myNav.classList.toggle("active");
+    hamMenu.classList.toggle("fa-times");
+});
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", () => {
+        myNav.classList.remove("active");
+        hamMenu.classList.toggle("fa-times");
+    });
+});
+
+window.onscroll = function() {
+    /*---Scroll Indicator---*/
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+       document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scroll = (pos*100)/calcHeight;
+    document.getElementById("progress").style.width = `${scroll}%`;
+
+    /*---Scroll to Top Button---*/
+    let scrollTopButton = document.getElementById("scroll-top-btn");
+    if(pos>300) {
+        scrollTopButton.style.display = "grid";
+    } else {
+        scrollTopButton.style.display = "none";
+    }
+
+    scrollTopButton.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    /*---Sticky Navbar---*/
+    if(pos>0) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
